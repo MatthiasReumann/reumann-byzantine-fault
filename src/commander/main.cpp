1ab739d1dc6e95f85206ab51332c4ba17969e031
Matthias Reumann <reumann.matthias@student.htlwrn.ac.at>
2020-04-08 21:39:49 +0200

    Add cli11

--- src/commander/main.cpp
+++ src/commander/main.cpp
@@ -1,12 +1,21 @@
 #include <thread>
+#include "CLI11.hpp"
 #include "message_utils.h"
 
 using namespace std;
+using namespace CLI;
 
-int main() {
+int main(int argc, char* argv[]) {
+    bool isTraitor{false};
+
+    App app{"Byzantine Fault"};
+    app.add_flag("-t, --traitor", isTraitor, "Is traitor");
+    CLI11_PARSE(app, argc, argv);
+
     thread l1{message_utils::send_message, message::ATTACK, "10000"};
-    thread l2{message_utils::send_message, message::RETREAT, "10001"};
+    thread l2{message_utils::send_message, isTraitor ? message::RETREAT : message::ATTACK, "10001"};
 
     l1.join(),
     l2.join();
+
 }
--- src/leutnant/main.cpp
+++ src/leutnant/main.cpp
@@ -2,14 +2,16 @@
 #include <string>
 #include <future>
 #include "asio.hpp"
+#include "CLI11.hpp"
 #include "message_utils.h"
 
 using namespace std;
 using namespace asio::ip;
+using namespace CLI;
 
 class Lieutenant{
     public:
-        Lieutenant(unsigned short port, string neighbour_port) : port{port}, neighbour_port{neighbour_port}{}
+        Lieutenant(unsigned short port, string neighbour_port, bool isTraitor) : port{port}, neighbour_port{neighbour_port}, isTraitor{isTraitor}{}
 
         void operator()(){
             asio::io_context ctx;
@@ -24,6 +26,14 @@ class Lieutenant{
             string com_order = message_utils::recieve_message(move(com_sock));
             cout << "commanders order is: " << com_order << endl;
 
+            if(isTraitor){
+                if(com_order == message::ATTACK){
+                    com_order = message::RETREAT;
+                }else{
+                    com_order = message::ATTACK;
+                }
+            }
+
             thread t{message_utils::send_message, com_order, this->neighbour_port};
             t.detach();
 
@@ -35,13 +45,24 @@ class Lieutenant{
     private:
         unsigned short port;
         string neighbour_port;
+        bool isTraitor;
 };
 
-int main([[maybe_unused]]int argc, char* argv[]){
-    unsigned short p = stoi(argv[1]);
-    Lieutenant l{p, argv[2]};
+int main(int argc, char* argv[]){
+    bool isTraitor{false};
+    unsigned short port;
+    string neighbour_port;
 
-    thread treciever{l};
+    App app{"Byzantine Fault"};
+    app.add_flag("-t, --traitor", isTraitor, "Is traitor");
+    app.add_option("-p, --port", port, "Lieutnant port");
+    app.add_option("-n, --neighbour", neighbour_port, "Neighbour lieutnant port");
+
+    CLI11_PARSE(app, argc, argv);
+
+    Lieutenant l{port, neighbour_port, isTraitor};
+
+    thread treciever{l}; //thread is basically useless here
 
     treciever.join();
 }
